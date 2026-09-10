c399fd9dcd9c9df5766af77595034fb535b41825
Angi-Adema <dr.adema@gmail.com>
2026-09-10 11:41:27 -0600

    created a dictionary to hold the model info including designed classes and attributes.

---
+++ pseudocode_script.py
@@ -0,0 +1,9 @@
+# Create a Python script that prints pseudocode based on the Vehicle database design model.
+
+# Define a dictionary that represents stores model info, classes and attributes shown in the Vehicle model
+vehicle_model = {
+    "Vehicle": ["vehicleID", "make"],
+    "Car": ["numberOfDoors", "isAllWheelDrive"],
+    "Truck": ["bedLength", "is4x4"],
+    "Driver": ["driverID", "driverName"]
+}
